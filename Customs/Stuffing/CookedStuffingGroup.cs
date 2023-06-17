f6b4ef45fc9f4f183963a4540a895d4613a6408a
09awill <50367356+09awill@users.noreply.github.com>
2023-06-17 13:47:01 +0100

    Initial commit

---
+++ Customs/Stuffing/CookedStuffingGroup.cs
@@ -0,0 +1,79 @@
+﻿using Kitchen;
+using KitchenData;
+using KitchenEasyStuff;
+using KitchenLib.Customs;
+using KitchenLib.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace EasyStuff.Customs
+{
+    internal class CookedStuffingGroup : CustomItem
+    {
+        public override string UniqueNameID => "CookedStuffingGroup";
+        public override GameObject Prefab => Mod.Bundle.LoadAsset<GameObject>("Stuffing");
+        public override ItemCategory ItemCategory => ItemCategory.Generic;
+        public override ItemStorage ItemStorageFlags => ItemStorage.StackableFood;
+        public override ItemValue ItemValue => ItemValue.Medium;
+        public override string ColourBlindTag => "Stuff";
+        public override bool SplitByCopying => true;
+        public override float SplitSpeed => 2.5f;
+        public override Item SplitSubItem => Mod.StuffingPortion;
+        public override List<Item> SplitDepletedItems => new List<Item> { Mod.StuffingPortion };
+        public override int SplitCount => 2;
+
+        public override void OnRegister(Item gameDataObject)
+        {
+            Prefab.ApplyMaterialToChild("BreadcrumbsBowl/Cylinder", "Metal Dark");
+            for(int i = 1; i < 4; i++)
+            {
+                for(int j = 1; j < 3; j++)
+                {
+                    Prefab.ApplyMaterialToChild($"Portion{i}/Ice Cream{j}", "Bread - Inside Cooked");
+                }
+            }
+
+
+            var view = Prefab.AddComponent<CookedStuffingGroupView>();
+
+            GameObject[] list = new GameObject[]
+            {
+                Prefab.GetChild("Portion2"),
+                Prefab.GetChild("Portion3"),
+            };
+            view.m_Objects = list;
+        }
+    }
+
+    public class CookedStuffingGroupView : ObjectsSplittableView
+    {
+        public GameObject[] m_Objects;
+        private ViewData m_Data;
+        private bool m_RunOnce = false;
+        protected override void UpdateData(ViewData data)
+        {
+            if (m_RunOnce && data.Remaining == 0)
+            {
+                return;
+            }
+            else
+            {
+
+                for (int i = 0; i < m_Objects.Length; i++)
+                {
+
+                    GameObject gameObject = m_Objects[i];
+                    gameObject.SetActive(i < data.Remaining);
+                }
+
+                m_Data = data;
+                m_RunOnce = true;
+            }
+        }
+
+    }
+}
---
+++ Mod.cs
@@ -0,0 +1,144 @@
+﻿using IngredientLib;
+using Kitchen;
+using KitchenData;
+using KitchenLib;
+using KitchenLib.Customs;
+using KitchenLib.Event;
+using KitchenLib.References;
+using KitchenLib.Utils;
+using KitchenMods;
+using System.Linq;
+using System.Reflection;
+using UnityEngine;
+using System.Collections.Generic;
+using EasyStuff.Customs;
+
+// Namespace should have "Kitchen" in the beginning
+namespace KitchenEasyStuff
+{
+    public class Mod : BaseMod, IModSystem
+    {
+        // GUID must be unique and is recommended to be in reverse domain name notation
+        // Mod Name is displayed to the player and listed in the mods menu
+        // Mod Version must follow semver notation e.g. "1.2.3"
+        public const string MOD_GUID = "Madvion.Plateup.EasyStuff";
+        public const string MOD_NAME = "Easy Stuff";
+        public const string MOD_VERSION = "0.1.0";
+        public const string MOD_AUTHOR = "Madvion";
+        public const string MOD_GAMEVERSION = ">=1.1.4";
+        // Game version this mod is designed for in semver
+        // e.g. ">=1.1.3" current and all future
+        // e.g. ">=1.1.3 <=1.2.3" for all from/until
+
+        // Boolean constant whose value depends on whether you built with DEBUG or RELEASE mode, useful for testing
+#if DEBUG
+        public const bool DEBUG_MODE = true;
+#else
+        public const bool DEBUG_MODE = false;
+#endif
+
+        public static AssetBundle Bundle;
+
+        internal static Item Onion => GetExistingGDO<Item>(ItemReferences.Onion);
+        internal static Item Flour => GetExistingGDO<Item>(ItemReferences.Flour);
+        internal static Item Water => GetExistingGDO<Item>(ItemReferences.Water);
+
+
+        internal static Item StuffingPortion => GetExistingGDO<Item>(ItemReferences.Stuffing);
+        internal static Item StuffingRaw => GetExistingGDO<Item>(ItemReferences.StuffingRaw);
+        internal static Item CookedStuffingGroup => GetModdedGDO<Item, CookedStuffingGroup>();
+
+
+
+
+
+
+
+
+        internal static Process Cook => GetExistingGDO<Process>(ProcessReferences.Cook);
+
+        internal static Process Chop => GetExistingGDO<Process>(ProcessReferences.Chop);
+        internal static Process RequireOven => GetExistingGDO<Process>(ProcessReferences.RequireOven);
+
+        public Mod() : base(MOD_GUID, MOD_NAME, MOD_AUTHOR, MOD_VERSION, MOD_GAMEVERSION, Assembly.GetExecutingAssembly()) { }
+
+        protected override void OnInitialise()
+        {
+            LogWarning($"{MOD_GUID} v{MOD_VERSION} in use!");
+        }
+
+        private void AddGameData()
+        {
+            LogInfo("Attempting to register game data...");
+
+            AddGameDataObject<CookedStuffingGroup>();
+
+            LogInfo("Done loading game data.");
+        }
+
+        protected override void OnUpdate()
+        {
+        }
+
+        protected override void OnPostActivate(KitchenMods.Mod mod)
+        {
+            // TODO: Uncomment the following if you have an asset bundle.
+            // TODO: Also, make sure to set EnableAssetBundleDeploy to 'true' in your ModName.csproj
+
+            LogInfo("Attempting to load asset bundle...");
+            Bundle = mod.GetPacks<AssetBundleModPack>().SelectMany(e => e.AssetBundles).First();
+            LogInfo("Done loading asset bundle.");
+
+            // Register custom GDOs
+            AddGameData();
+
+            // Perform actions when game data is built
+            Events.BuildGameDataEvent += delegate (object s, BuildGameDataEventArgs args)
+            {
+                
+                Item stuffingRaw = args.gamedata.Get<Item>(ItemReferences.StuffingRaw);
+                for(int i = 0; i < stuffingRaw.DerivedProcesses.Count; i++)
+                {
+                    if (stuffingRaw.DerivedProcesses[i].Result == StuffingPortion)
+                    {
+                        Item.ItemProcess proc = stuffingRaw.DerivedProcesses[i];
+                        proc.Result = CookedStuffingGroup;
+                        stuffingRaw.DerivedProcesses[i] = proc;
+                    }
+                }
+                
+            };
+        }
+
+        private static T1 GetModdedGDO<T1, T2>() where T1 : GameDataObject
+        {
+            return (T1)GDOUtils.GetCustomGameDataObject<T2>().GameDataObject;
+        }
+        private static T GetExistingGDO<T>(int id) where T : GameDataObject
+        {
+            return (T)GDOUtils.GetExistingGDO(id);
+        }
+        internal static T Find<T>(int id) where T : GameDataObject
+        {
+            return (T)GDOUtils.GetExistingGDO(id) ?? (T)GDOUtils.GetCustomGameDataObject(id)?.GameDataObject;
+        }
+
+        internal static T Find<T, C>() where T : GameDataObject where C : CustomGameDataObject
+        {
+            return GDOUtils.GetCastedGDO<T, C>();
+        }
+
+        internal static T Find<T>(string modName, string name) where T : GameDataObject
+        {
+            return GDOUtils.GetCastedGDO<T>(modName, name);
+        }
+        #region Logging
+        public static void LogInfo(string _log) { Debug.Log($"[{MOD_NAME}] " + _log); }
+        public static void LogWarning(string _log) { Debug.LogWarning($"[{MOD_NAME}] " + _log); }
+        public static void LogError(string _log) { Debug.LogError($"[{MOD_NAME}] " + _log); }
+        public static void LogInfo(object _log) { LogInfo(_log.ToString()); }
+        public static void LogWarning(object _log) { LogWarning(_log.ToString()); }
+        public static void LogError(object _log) { LogError(_log.ToString()); }
+        #endregion
+    }
+}
